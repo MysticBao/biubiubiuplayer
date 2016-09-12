fb034bf68987c66a1acf7d6ab16e5f703bbcc70d
Mystic Weng <710120644@qq.com>
2016-09-12 22:20:40 +0800

    commit default window

---
+++ main.js
@@ -0,0 +1,24 @@
+const electron = require('electron')
+const BrowserWindow = electron.BrowserWindow
+const app = electron.app
+
+let mainWindow
+
+function createWindow(){
+  mainWindow = new BrowserWindow({width: 800,height: 600})
+  mainWindow.loadURL(`file://${__dirname}/view/main/index.html`)
+
+  mainWindow.on('closed',function(){
+    mainWindow=null;
+  })
+}
+
+app.on('ready', ()=>{
+  createWindow()
+})
+
+app.on('activate',()=> {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
