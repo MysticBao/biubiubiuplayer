9316fde0ca267ef513ce7ac759b7e3e1c263a4ce
Mystic Weng <710120644@qq.com>
2016-09-20 23:55:53 +0800

    add new file

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let mainWindow
 
 function createWindow(){
   mainWindow = new BrowserWindow({width: 800,height: 600})
-  mainWindow.loadURL(`file://${__dirname}/view/main/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/src/main/index.html`)
 
   mainWindow.on('closed',function(){
     mainWindow=null;
