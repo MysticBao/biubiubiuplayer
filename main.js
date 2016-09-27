4bf87bbd1baa08026ccf995fbf8ca9f8065358cb
Mystic Weng <710120644@qq.com>
2016-09-28 00:06:35 +0800

    update button style

--- main.js
+++ main.js
@@ -17,9 +17,9 @@ function createWindow(){
 
 app.on('ready', ()=>{
   createWindow()
-  mainWindow.openDevTools();
+  //mainWindow.openDevTools();
 
-  //add ipc to open file and pass the file path to render ipc 
+  //add ipc to open file and pass the file path to render ipc
   ipcMain.on('popup-open-file-dialog', (event) => {
   	var file = dialog.showOpenDialog({properties:['openFile']});
 	console.log('select file: ' + file);
